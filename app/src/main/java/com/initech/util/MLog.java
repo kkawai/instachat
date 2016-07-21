f61839f43d98318db6becbcb596032fb93f3738f
vevo-kevin-kawai <kevin.kawai@vevo.com>
2016-07-21 02:48:51 -0700

    added volley

---
+++ app/src/main/java/com/initech/util/MLog.java
@@ -0,0 +1,74 @@
+package com.initech.util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MLog {
+
+    private static boolean globalEnabled = true;
+
+    private MLog() {
+    }
+
+    public static void setGlobalEnabled(final boolean f) {
+        globalEnabled = f;
+        if (!f) {
+            Logger.getLogger("com.instachat").setLevel(Level.OFF);
+        } else {
+            Logger.getLogger("com.instachat").setLevel(Level.INFO);
+        }
+    }
+
+    public static boolean getGlobalEnabled() {
+        return globalEnabled;
+    }
+
+    public static void enable(String tag) {
+    }
+
+    public static void disable(String tag) {
+    }
+
+    private static boolean isEnabled(String tag) {
+        return globalEnabled;
+    }
+
+    public static void i(String tag, Object... vals) {
+        if (isEnabled(tag)) {
+            Logger.getLogger(tag).log(Level.INFO, buildString(vals));
+        }
+    }
+
+    public static void w(String tag, Object... vals) {
+        if (isEnabled(tag)) {
+            Logger.getLogger(tag).log(Level.WARNING, buildString(vals));
+        }
+    }
+
+    public static void e(String tag, Object... vals) {
+        if (isEnabled(tag)) {
+            Logger.getLogger(tag).log(Level.SEVERE, buildString(vals));
+        }
+    }
+
+    public static void e(String tag, String log, Throwable t) {
+        if (isEnabled(tag)) {
+            Logger logger = Logger.getLogger(tag);
+            logger.log(Level.SEVERE, log);
+            logger.log(Level.SEVERE, t.toString());
+            StackTraceElement[] eles = t.getStackTrace();
+            for (StackTraceElement ele : eles) {
+                logger.log(Level.SEVERE, ele.toString());
+            }
+        }
+    }
+
+    private static String buildString(Object... strings) {
+        final StringBuilder sb = new StringBuilder();
+        for (Object s : strings) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+}
