cacf1a3fa61425d37da3c7845d64a9a656bec6e4
vevo-kevin-kawai <kevin.kawai@vevo.com>
2016-10-19 09:07:42 -0700

    minor: refactor

--- app/src/main/java/com/instachat/android/adapter/MessagesRecyclerAdapter.java
+++ app/src/main/java/com/instachat/android/adapter/MessagesRecyclerAdapter.java
@@ -143,7 +143,7 @@ public class MessagesRecyclerAdapter<T, VH extends RecyclerView.ViewHolder> exte
 
     private MessageViewHolder createMessageViewHolder(ViewGroup parent, int viewType) {
         if (viewType == ITEM_VIEW_TYPE_WEB_LINK) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_content, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.web_clipping_content, parent, false);
             final MessageViewHolder holder = new MessageViewHolder(view);
             View.OnClickListener webLinkClickListener = new View.OnClickListener() {
                 @Override
