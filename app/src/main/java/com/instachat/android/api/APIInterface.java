0b008e738206690a2130eda8d514ac7616010522
kkawai <kkawai2@gmail.com>
2017-09-22 11:11:29 -0700

    minor import fix

--- app/src/main/java/com/instachat/android/api/APIInterface.java
+++ app/src/main/java/com/instachat/android/api/APIInterface.java
@@ -2,7 +2,6 @@ package com.instachat.android.api;
 
 import com.instachat.android.model.User;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
